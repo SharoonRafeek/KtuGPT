b77ca47903e2aeca5eb775ae306751598794f064
student <sharoonrafeek>
2024-02-29 10:45:05 +0530

    Started

--- app/page.js
+++ app/page.js
@@ -1,10 +1,11 @@
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
-      <h1>
-        Hello, There
-      </h1>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="w-1/4 h-full bg-inherit">
+        History
+      </div>
     </main>
   );
 }
