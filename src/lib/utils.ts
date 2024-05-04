66ef844c44993e1eed40f9066fd5b8dda2a1fb7e
SharoonRafeek <sharoonrafeek@gmail.com>
2024-05-04 12:11:20 +0530

    Update title in layout.tsx and change assistant name in utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -47,7 +47,7 @@ export const initialMessages: Message[] = [
     role: "assistant",
     id: "0",
     content:
-      "Hi! I am your PDF assistant. I am happy to help with your questions about Data Structure and Algorithms.",
+      "Hi! I am KtuGPT. I am happy to help with your questions about Data Structure and Algorithms.",
   },
 ];
 
@@ -55,7 +55,6 @@ interface Data {
   sources: string[];
 }
 
-// Maps the sources with the right ai-message
 export const getSources = (data: Data[], role: string, index: number) => {
   if (role === "assistant" && index >= 2 && (index - 2) % 2 === 0) {
     const sourcesIndex = (index - 2) / 2;
